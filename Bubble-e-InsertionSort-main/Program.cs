fbc61c763a1b9918e2dae4af140224332d5047de
Miguel Brugnago <miguelhauck8@gmail.com>
2025-12-01 16:27:02 -0300

    sla

---
+++ Bubble-e-InsertionSort-main/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using BubbleSort;
+
+Console.WriteLine("Ordenação com Bubble Sort");
+Console.WriteLine("");
+
+string[] arrStr = new string[] { "B", "D", "A", "C" };
+
+foreach (var str in arrStr) Console.Write($"[{str}] ");
+
+var arrOrdered = BubbleSort.BubbleSort.Sort<string>(arrStr);
+
+Console.WriteLine("");
+Console.WriteLine("Vetor Ordenado");
+Console.WriteLine("");
+foreach (var str in arrOrdered) Console.Write($"[{str}] ");
